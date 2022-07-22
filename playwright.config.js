3071a57cd0d2ca7a24314a0703bd8f9e5338a300
Nitya Narasimhan <nitya@users.noreply.github.com>
2022-07-22 10:59:52 -0400

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -31,8 +31,8 @@ const config = {
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['html'],
-    ['json', { outputFile: 'report.json' }]
+    ['html', { outputFolder: 'html' }],
+    ['json', { outputFolder: 'json', outputFile: 'report.json' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
